c5fd937958bff060761ffb0d402cd37be5fbb759
amin <aungkhantmin.dev@gmail.com>
2024-10-21 19:11:55 +0300

    fixed

--- LinesPackage.go
+++ LinesPackage.go
@@ -5,7 +5,7 @@ func LinesPackages(c rune, Lines []string) []string {
 	start, end := Limits(c)
 	var result []string
 
-	for i := start - 1; i <= end-1; i++ {
+	for i := start; i <= end; i++ {
 		result = append(result, Lines[i])
 	}
 
--- Lmits.go
+++ Lmits.go
@@ -3,7 +3,7 @@ package main
 func Limits(c rune) (start, end int) {
 
 	n := int(c - ' ')
-	start = 9*n + 2
+	start = 9*n + 1
 	end = start + 7
 
 	return start, end
