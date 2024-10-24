b2376ca954b33d5ac1224de7604044c217959f41
amin <aungkhantmin.dev@gmail.com>
2024-10-24 17:08:27 +0300

    24.10.2024

--- Lmits.go
+++ Lmits.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func Limits(c rune) (start, end int) {
+
+	if c <= ' ' || c >= '~' {
+		fmt.Println("The input you just put is not a printable character or a character outside of ascii table")
+		os.Exit(1)
+	}
 
 	n := int(c - ' ')
 	start = 9*n + 1
